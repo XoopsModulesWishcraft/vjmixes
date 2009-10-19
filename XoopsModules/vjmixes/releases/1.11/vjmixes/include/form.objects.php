a49f578a0a763936846bbe59a17e7d3e5a7f744e
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-10-19 22:40:25 +0000

    VJ Mixes 1.11 - Xoops Module for 2.3.x, 2.4.x

---
+++ XoopsModules/vjmixes/releases/1.11/vjmixes/include/form.objects.php
@@ -0,0 +1,7 @@
+<?php
+	
+	include_once XOOPS_ROOT_PATH . '/class/xoopsformloader.php';
+	include_once XOOPS_ROOT_PATH . '/modules/vjmixes/class/formselectcategory.php';
+	include_once XOOPS_ROOT_PATH . '/modules/tag/include/formtag.php';
+	
+?>
